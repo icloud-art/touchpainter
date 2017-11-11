c6f1bfabb21cec3594fdbf0e6c8a7824d96b5412
charlese <iosdeveloperleo@163.com>
2017-11-11 19:23:15 +0800

    first commit

---
+++ TouchPainter/TouchPainter/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TouchPainter
+//
+//  Created by Charles on 2017/11/11.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TouchPainter/TouchPainter/MainViewController.h
@@ -0,0 +1,13 @@
+//
+//  MainViewController.h
+//  TouchPainter
+//
+//  Created by Charles on 2017/11/11.
+//  Copyright © 2017年 Charles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainViewController : UIViewController
+
+@end
